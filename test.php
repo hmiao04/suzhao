0b9dcabd6b386f6a258366a07420c7727ce66a44
hmiao04 <hmiao04@hotmail.com>
2017-07-12 15:34:02 -0600

    First time i add test.php file

---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miao
+ * Date: 20/06/17
+ * Time: 1:11 PM
+ */
+
+require 'usr/controller/FrontPage/PictureController.php';
+require 'usr/var/env.php';
+
+$PC = new PictureController();
+echo 'here';
+
+?>
